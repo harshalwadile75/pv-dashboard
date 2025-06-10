92557f26b3a4acaee27efa41d5a24af482c476c8
harshalwadile75 <123146573+harshalwadile75@users.noreply.github.com>
2025-06-10 15:09:44 -0400

    Update weather.py

--- utils/weather.py
+++ utils/weather.py
@@ -1,6 +1,16 @@
+import pandas as pd
 from pvlib.location import Location
 
 def get_tmy_data(lat, lon):
     loc = Location(lat, lon, 'UTC')
-    tmy_data, meta = loc.get_clearsky().resample('M').mean(), {'latitude': lat, 'longitude': lon}
+    
+    # Use one year of hourly timestamps
+    times = pd.date_range('2023-01-01', '2023-12-31 23:00', freq='H', tz='UTC')
+    
+    # Generate clear sky data for this year
+    clearsky = loc.get_clearsky(times)
+    
+    # Return monthly averages
+    tmy_data = clearsky.resample('M').mean()
+    
     return tmy_data
